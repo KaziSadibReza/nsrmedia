b1c39c872edec51f0d0057c80aae94d9396117e0
Kazi Sadib Reza <kazisadibreza@gmail.com>
2025-12-01 14:45:40 +0600

    update

--- kadence-child/include/admin/works-post-type.php
+++ kadence-child/include/admin/works-post-type.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function remove_kadence_settings_for_works_slider() {
     // Replace 'your_cpt_slug' with the actual slug of your custom post type.
-    remove_meta_box( '_kad_classic_meta_control', 'works_slider', 'side' );
+    remove_meta_box( '_kad_classic_meta_control', 'works-sider', 'side' );
 }
 add_action( 'add_meta_boxes', 'remove_kadence_settings_for_works_slider', 20 );
 
@@ -19,7 +19,7 @@ add_action( 'add_meta_boxes', 'remove_kadence_settings_for_works_slider', 20 );
  * @return void
  */
 add_action('do_meta_boxes', function() {
-    remove_meta_box('postimagediv', 'works_slider', 'side');
+    remove_meta_box('postimagediv', 'works-sider', 'side');
 });
 
 
@@ -29,6 +29,6 @@ add_action('do_meta_boxes', function() {
  * @return void
  */
 function remove_editor_works_slider() {
-    remove_post_type_support( 'works_slider', 'editor' );
+    remove_post_type_support( 'works-sider', 'editor' );
 }
 add_action( 'admin_menu' , 'remove_editor_works_slider' );
